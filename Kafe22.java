2740be35e1eadf01d1948280e0e53845f692de0c
qulbyazz <qulbyazzumy@gmail.com>
2024-11-21 08:14:31 +0700

    percobaan 1 pertanyaan

--- Kafe22.java
+++ Kafe22.java
@@ -1,6 +1,5 @@
 public class Kafe22 {
-
-    public static void menu(){
+    public static void main(String[] args) {
         System.out.println("====MENU KAFE RESTO====");
         System.out.println("1. Kopi Hitam - Rp. 15.000");
         System.out.println("2. Capuccino - Rp. 20.000");
@@ -10,8 +9,5 @@ public class Kafe22 {
         System.out.println("6. Mie Goreng - Rp. 18.000");
         System.out.println("==========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan");
-    }
-    public static void main(String[] args) {
-        menu();
     }
 }
